1d8c74f6934566705ee11037eacaceab570c2e3a
wyttttt <1301263327@qq.com>
2021-05-24 09:59:43 +0800

    Signed-off-by: 后台管理系统版本2

--- 后台管理系统版本2/middleware/cache-control.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-module.exports = async (c, next) => {
-
-  //请求缓存30秒
-  c.setHeader('cache-control', 'public, max-age=30')
-
-  await next()
-}
--- 后台管理系统版本2/middleware/test-logger.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-module.exports = async (c, next) => {
-
-  console.log(c.method, c.path, c.routepath, c.headers['user-agent'] || '')
-
-  await next()
-
-}
